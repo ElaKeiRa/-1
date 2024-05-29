0b16ed3cc251a20b1d2c4aeda90fec2528397a6c
ElaKeiRa <66elka66@mail.ru>
2024-05-29 23:16:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+print(len("Dania"))
+
+first = 5
+second = 7
+third = 9
+summa = first + second
+diff = first - second
+mi = first + second + third
+mean = mi / 3
+print(summa)
+print(diff)
+print(mean)
+
+first_string = "Вторник"
+second_string = "Понедельник"
+print(second_string,first_string, sep =', ')
+
+
+a = 3
+b = 7
+c = 9
+f = (((a * b) + (a * c)) ** 3) // 2
+print(f)
